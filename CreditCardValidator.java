94fde12df37537d20b89ccb9ed371770fd8ea6f2
crazyminerparody <wsholanky@gmail.com>
2026-01-13 11:20:28 +0100

    Initail commit

---
+++ CreditCardValidator.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+
+public class CreditCardValidator {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter your credit card number: ");
+        String cardNumber = input.nextLine();
+
+        int length = cardNumber.length();
+
+        int sumEven = 0;
+        int sumOdd = 0;
+
+        String cardType = "Invalid card";
+        String cardValidity;
+
+        for (int i = length - 2; i >= 0; i -= 2) {
+            int digit = cardNumber.charAt(i) - '0';
+            digit *= 2;
+
+            if (digit > 9) {
+                digit -= 9;
+            }
+
+            sumEven += digit;
+        }
+
+        for (int i = length - 1; i >= 0; i -= 2) {
+            int digit = cardNumber.charAt(i) - '0';
+            sumOdd += digit;
+        }
+
+        int total = sumEven + sumOdd;
+
+        if (total % 10 == 0) {
+            cardValidity = "Valid";
+        } else {
+            cardValidity = "Invalid";
+        }
+
+        if (cardNumber.startsWith("4")) {
+            cardType = "Visa Card";
+        } 
+        else if (cardNumber.startsWith("5")) {
+            cardType = "MasterCard";
+        } 
+        else if (cardNumber.startsWith("6")) {
+            cardType = "Discover Card";
+        } 
+        else if ((cardNumber.startsWith("34") || cardNumber.startsWith("37")) && length == 15) {
+            cardType = "American Express Card";
+        }
+
+        System.out.println("\nCredit Card Type: " + cardType);
+        System.out.println("Credit Card Number: " + cardNumber);
+        System.out.println("Credit Card Length: " + length);
+        System.out.println("Credit Card Validity: " + cardValidity);
+
+        input.close();
+    }
+}
+
